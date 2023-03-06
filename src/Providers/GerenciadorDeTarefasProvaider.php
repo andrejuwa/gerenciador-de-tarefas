2631eabbf5b4b53f8a63c582986df402d7ab8c6f
andrejuwa <andre.wagner@acto.com.br>
2023-03-06 12:38:53 -0300

    web

--- src/Providers/GerenciadorDeTarefasProvaider.php
+++ src/Providers/GerenciadorDeTarefasProvaider.php
@@ -10,11 +10,11 @@ class GerenciadorDeTarefasProvaider extends ServiceProvider {
 
     public function register()
     {
-        // Registro de bindings, serviços ou outros componentes do pacote
+
     }
 
     public function boot()
     {
-        // Registre rotas, visualizações, arquivos de configuração e outras configurações do pacote aqui
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
     }
 }
